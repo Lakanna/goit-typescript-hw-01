bebd3d8383dd2d163c2cf6801ac084b4827af02e
Hanna Lakus <annalakus@ukr.net>
2024-10-02 23:27:06 +0100

    basic part done

---
+++ src/basic/3.ts
@@ -0,0 +1,9 @@
+let someValue: string | number;
+someValue = 12;
+someValue = "TS";
+
+type EnableOrDisable = "enable" | "disable";
+let toggle: EnableOrDisable;
+
+toggle = "disable";
+toggle = "enable";
